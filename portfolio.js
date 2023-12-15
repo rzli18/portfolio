58be3bc4d847ac008083215f9227e8e136249e10
rzli18 <146799761+rzli18@users.noreply.github.com>
2023-12-14 22:50:40 -0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+function navigateToTF() {
+    window.location.href = 'tf.html';
+}
+
+function navigateToUCEN() {
+    window.location.href = 'ucen.html';
+}
+
+function goHome() {
+    window.location.href = 'index.html';
+}
